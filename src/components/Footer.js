1fceab08e970a87b2a7ccc92cc3bf9203a1591a7
Mai Bui <maipbui98@gmail.com>
2021-12-27 19:14:42 -0500

    update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,21 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div style={{position:"fixed", bottom:"5vh", left:'50vw', transform:'translateX(-50%)'}}>
-        <div color="blue" >
-            <div>
-                &copy; {new Date().getFullYear()} Copyright: <a href="https://github.com/maipbui/"> Mai Bui</a> &amp; <a href="https://github.com/loctran15"> Loc Tran </a>
-            </div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        minHeight: "100px",
+        alignItems: "center",
+      }}
+    >
+      <div color="blue">
+        <div>
+          &copy; {new Date().getFullYear()} Copyright:{" "}
+          <a href="https://github.com/maipbui/"> Mai Bui</a> &amp;{" "}
+          <a href="https://github.com/loctran15"> Loc Tran </a>
         </div>
+      </div>
     </div>
   );
 }
